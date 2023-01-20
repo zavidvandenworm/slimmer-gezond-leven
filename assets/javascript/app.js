56cd544fc5edc6015da456eedd9ec79cdcd3e664
MAB <zavidvandenworm2@gmail.com>
2023-01-20 18:52:34 +0100

    dingen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,19 @@ let pageview = document.getElementById("pageview");
 document.getElementById("page_back").onclick = function() {
 	pageview.contentWindow.history.back();
 }
+
+document.getElementById("page_forward").onclick = function() {
+	pageview.contentWindow.history.forward();
+}
+
+document.getElementById("page_home").onclick = function() {
+	pageview.contentWindow.location = "./assets/pages/keuzepagina.html";
+}
+
+document.getElementById("page_debug").onclick = function() {
+	pageview.contentWindow.location = "./assets/pages/dev.html";
+}
+
+document.getElementById("page_createaccount").onclick = function() {
+	pageview.contentWindow.location = "./assets/pages/accountaanmaken.html";
+}
