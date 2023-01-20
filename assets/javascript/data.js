93175fdccb8b1b9039d5e940225910eb78877ab7
MAB <zavidvandenworm2@gmail.com>
2023-01-20 19:57:17 +0100

    account maken werkt soort van

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,2 +1,15 @@
+let user_data = {};
+
 if(localStorage.getItem("sgl_loggedin") === null) {
+    localStorage.setItem("sgl_school", "N/A");
+    localStorage.setItem("sgl_classcode", "N/A");
+    localStorage.setItem("sgl_name", "N/A");
+    localStorage.setItem("sgl_classcode", "N/A");
+    localStorage.setItem("sgl_name", "N/A");
+    localStorage.setItem("sgl_birthday", "N/A");
+    localStorage.setItem("sgl_gender", "N/A");
+    localStorage.setItem("sgl_weight", "N/A");
+    localStorage.setItem("sgl_trainingintensity", "N/A");
+    localStorage.setItem("sgl_idealweight", "N/A");
+    localStorage.setItem("sgl_dailycalories", "N/A");
 }
