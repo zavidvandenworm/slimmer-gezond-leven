c4ba3e5d6efea9f714ed479ce6018bf0674e30c5
Zavid <84294518+zavid502@users.noreply.github.com>
2023-01-25 09:34:51 +0100

    Update recepten.js

--- assets/javascript/recepten.js
+++ assets/javascript/recepten.js
@@ -1,7 +1,7 @@
 document.getElementById("ontbijt_button").onclick = function() {
     if(
-        parseInt(localStorage.getItem("sgl_weight")) > 
-        parseInt(localStorage.getItem("sgl_ideal_weight"))
+        parseInt(localStorage.getItem("sgl_weight")) >
+        parseInt(localStorage.getItem("sgl_idealweight"))
     ){
         location.href = "./pages_recepten/page_ontbijt_afvallen.html";
         
